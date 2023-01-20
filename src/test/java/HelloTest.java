772ad63a3ec9f9790fa8149792ea95540d1f7ffc
19786 <agil83p@yahoo,com>
2023-01-20 17:44:01 -0500

    added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,7 @@ public class HelloTest {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("About share project on github");
     }
 }
